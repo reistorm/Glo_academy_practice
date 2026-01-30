5457c3c781c91d7c139bac31a1c72c6aff4dc88f
reistorm <rysaeva96@mail.ru>
2026-01-30 17:01:29 +0300

    task

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = '';
+let screens = [];
+let screenPrice = 0;
+let rollbacke = false;
+let fullPrice = 0;
+let adaptive = true;
+
+console.log(alert('Hello there'));
+console.log('Hello world');
