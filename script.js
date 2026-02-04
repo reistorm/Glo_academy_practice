e3d71d66d37d7a37f2e8d2e9f88cbd9aa13138e8
Eliza X. <rysaeva96@mail.ru>
2026-02-04 11:38:25 +0300

    выполнено дз3 с изменением на чистую функцию

--- script.js
+++ script.js
@@ -5,43 +5,39 @@ let rollbacke = 50;
 let fullPrice = 500000;
 let adaptive = true;
 
-
-
 title = prompt('Как называется ваш проект?');
-//console.log(`Проект - ${title}`);
 
 screens = prompt('Какие типы экранов нужно разработать?')
-//console.log(`Типы экранов: ${screens}`);
 
 screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'))
-//console.log(`Стоимость работы: ${screenPrice}`);
 
 adaptive = confirm(`Нужен ли адаптив на сайте?`)
 
 const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = parseFloat(prompt("Сколько это будет стоить?")); 
+const servicePrice1 = parseFloat(prompt("Сколько это будет стоить?"));
 
-const service2 = prompt("Какой еще дополнительный тип услуги нужен?"); 
-const servicePrice2 = parseFloat(prompt("Сколько это будет стоить?")); 
+const service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+const servicePrice2 = parseFloat(prompt("Сколько это будет стоить?"));
 
 
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-
 const servicePercentPrice = Math.ceil(fullPrice - rollbacke);
 console.log("Стоимость за вычетом отката:", servicePercentPrice);
 
-if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%');
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-    console.log('Даем скидку в 5%');
-} else if (fullPrice < 15000 && fullPrice > 0) {
-    console.log('Скидка не предусмотрена');
-} else if (fullPrice <= 0) {
-    console.log('Что-то пошло не так');
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return "Даем скидку в 10%"
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%"
+    } else if (price < 15000 && price > 0) {
+        return "Скидка не предусмотрена"
+    } else if (price <= 0) {
+        return "Что-то пошло не так"
+    }
 }
 
-
+console.log(getRollbackMessage(fullPrice));
 
 
 
