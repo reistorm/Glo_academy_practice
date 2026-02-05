f07ad278c6335c1908fbfb09c3f57a26ab8929ae
Eliza X. <rysaeva96@mail.ru>
2026-02-05 14:07:47 +0300

    выполнено hard-lessons05-1

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let title = '';
-let screens = [];
-let screenPrice = 0;
-let rollbacke = false;
-let fullPrice = 0;
-let adaptive = true;
+// Придумать способ сохранять в переменную ответ пользователя после проверки на число именно как число при любом вводе. (с пробелами и без в переменную заносилось именно число) На данный момент проверка isNumber пропустит такой вариант "   123   " и именно это значение попадет в переменную. Необходимо это исправить. Так же учитывайте что человек может нажать отмену и в проверку уйдет значение NULL
 
 
-console.log(alert('Hello there'));
-console.log('Hello world');
+let answer;
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num) && isFinite(num))
+}
+
+const asking = function () {
+    do {
+        answer = prompt('Введите число');
+    } while (!isNumber(answer) || answer.trim() === "" || answer === null)
+    answer = parseFloat(answer)
+    console.log(answer + " - " + typeof answer);
+}
+
+asking();
